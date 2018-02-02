12fd3e3c82b4b135bdf5e20835cd7154fb85222a
Joao Luis <joao.luis.veronezzi@gmail.com>
2018-02-02 03:01:24 -0200

    WEbservice implementado

---
+++ README2.md
@@ -0,0 +1,5 @@
+sudo apt-get install python3-pip
+
+sudo pip3 install Flask-API
+sudo pip3 install Flask-Httpauth
+sudo pip3 install beautifulsoup4
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask_api import FlaskAPI
+from flask_httpauth import HTTPBasicAuth
+from flask import *
+from crawler import *
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+response = CrawlerResponse()
+app.config['JSON_AS_ASCII'] = False
+
+users = {
+    "globo": "globo",
+    "danny": "farias",
+    "alexandre": "prates",
+    "andreia": "almeida"
+}
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
+@app.route('/')
+@auth.login_required
+def index():
+    return response.getResponse()
+
+if __name__ == '__main__':
+    app.run()
